a0a7b6d2fb46378844d3f72ac633459eb4122e03
Mahammad Baghirzada <mahammadbaghirzada@gmail.com>
2024-08-02 18:21:56 +0400

    Add Project Members

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -37,5 +38,10 @@ class User extends Authenticatable
     public function projects(): HasMany
     {
         return $this->hasMany(Project::class, 'creator_id');
+    }
+
+    public function memberships(): BelongsToMany
+    {
+        return $this->belongsToMany(Project::class, Member::class);
     }
 }
