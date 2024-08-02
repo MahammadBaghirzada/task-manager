ba6f7bf5adecbe2fa1593d320f28284f16a6591f
Mahammad Baghirzada <mahammadbaghirzada@gmail.com>
2024-08-02 14:51:40 +0400

    Tasks That Belong to Users

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TaskFactory extends Factory
@@ -9,6 +10,7 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         return [
+            'creator_id' => User::query()->inRandomOrder()->first()->id,
             'title' => fake()->sentence(),
             'is_done' => fake()->boolean(),
         ];
--- database/migrations/2024_07_30_155352_create_tasks_table.php
+++ database/migrations/2024_07_30_155352_create_tasks_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('creator_id')->constrained('users');
             $table->string('title');
             $table->boolean('is_done')->default(false);
             $table->timestamps();
