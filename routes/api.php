e9164d09a6a6980b6486c9a1cf715355a3a09bbc
Mahammad Baghirzada <mahammadbaghirzada@gmail.com>
2024-08-02 16:39:49 +0400

    Creating and Updating Projects

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,8 +22,12 @@ Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
+
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+
     Route::apiResource('tasks', TaskController::class);
+
+    Route::apiResource('projects', ProjectController::class);
 });
