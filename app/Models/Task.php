382cb6012a9b71eb0ed17d966787cd68ec60491b
Mahammad Baghirzada <mahammadbaghirzada@gmail.com>
2024-08-07 17:48:37 +0400

    Filter Due Dates For Today and the Past

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -48,6 +49,15 @@ class Task extends Model
     public function scopeDueBetween(Builder $query, string $fromDate, string $toDate)
     {
         $query->whereBetween('due_at', [$fromDate, $toDate]);
+    }
+
+    public function scopeDue(Builder $query, string $filter)
+    {
+        if ($filter === 'today') {
+            $query->whereDate('due_at', Carbon::today());
+        } elseif ($filter === 'past') {
+            $query->whereDate('due_at', '<', Carbon::today());
+        }
     }
 
     public function creator(): BelongsTo
